ef7d8bd194492f403eec9a3b053dfe39e68e8f29
bsyso <bsyso@softserveinc.com>
2023-01-16 07:19:10 +0100

    added optimization

--- concurrency_2_coordination/src/main/java/org/example/ComplexCalculation.java
+++ concurrency_2_coordination/src/main/java/org/example/ComplexCalculation.java
@@ -2,12 +2,44 @@ package org.example;
 
 import java.math.BigInteger;
 
+/**
+ * Multithreaded Calculation
+ * In this coding exercise you will use all the knowledge from the previous lectures.
+ * Before taking the exercise make sure you review the following topics in particular:
+ * 1. Thread Creation - how to create and start a thread using the Thread class and the start() method.
+ * <p>
+ * 2. Thread Join - how to wait for another thread using the Thread.join() method.
+ * <p>
+ * In this exercise we will efficiently calculate the following result = base1 ^ power1 + base2 ^ power2
+ * <p>
+ * Where a^b means: a raised to the power of b.
+ * <p>
+ * For example 10^2 = 100
+ * <p>
+ * We know that raising a number to a power is a complex computation, so we we like to execute:
+ * <p>
+ * result1 = x1 ^ y1
+ * <p>
+ * result2 = x2 ^ y2
+ * <p>
+ * In parallel.
+ * <p>
+ * and combine the result in the end : result = result1 + result2
+ * <p>
+ * This way we can speed up the entire calculation.
+ * <p>
+ * Note :
+ * <p>
+ * base1 >= 0, base2 >= 0, power1 >= 0, power2 >= 0
+ */
 public class ComplexCalculation {
     public static void main(String[] args) throws InterruptedException {
-        var res = new ComplexCalculation().calculateResult(new BigInteger("2")
-                , new BigInteger("3")
-                , new BigInteger("4")
-                , new BigInteger("2"));
+        var res = new ComplexCalculation().calculateResult(
+                new BigInteger("2"),
+                new BigInteger("3"),
+                new BigInteger("4"),
+                new BigInteger("2")
+        );
 
         System.out.println(res);
     }
